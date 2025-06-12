3151b59735afb765c742e07eeb680cff01b755b3
StanceFreak <jombor.army67@gmail.com>
2025-06-12 21:38:41 +0700

    feat: added dependency injection using dagger hilt to the project

---
+++ app/src/main/java/com/stancefreak/pihakaseng/App.kt
@@ -0,0 +1,8 @@
+package com.stancefreak.pihakaseng
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+}
---
+++ app/src/main/java/com/stancefreak/pihakaseng/di/NetworkModule.kt
@@ -0,0 +1,61 @@
+package com.stancefreak.pihakaseng.di
+
+import com.stancefreak.pihakaseng.BuildConfig
+import com.stancefreak.pihakaseng.network.NetworkHelper
+import com.stancefreak.pihakaseng.network.NetworkService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRemoteService(retrofit: Retrofit): NetworkService = retrofit.create(NetworkService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideApiHelper(service: NetworkService): NetworkHelper {
+        return NetworkHelper(service)
+    }
+
+}
