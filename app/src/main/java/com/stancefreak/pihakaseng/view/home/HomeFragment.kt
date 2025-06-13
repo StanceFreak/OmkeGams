b8895b07e0655bfa4133ef7f4a46dac96ad873aa
StanceFreak <jombor.army67@gmail.com>
2025-06-13 21:31:58 +0700

    feat(home): added promo and now playing movies (rtl bug) using viewpager2 along with the movie genre list

--- app/src/main/java/com/stancefreak/pihakaseng/view/home/HomeFragment.kt
+++ app/src/main/java/com/stancefreak/pihakaseng/view/home/HomeFragment.kt
@@ -7,16 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import com.stancefreak.pihakaseng.R
 import com.stancefreak.pihakaseng.base.BaseFragment
 import com.stancefreak.pihakaseng.databinding.FragmentHomeBinding
+import com.stancefreak.pihakaseng.util.HorizontalMarginItemDecoration
+import com.stancefreak.pihakaseng.view.adapter.HomeGenreAdapter
+import com.stancefreak.pihakaseng.view.adapter.HomePlayingMovieAdapter
+import com.stancefreak.pihakaseng.view.adapter.HomePromoAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.abs
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHomeBinding::inflate) {
+class HomeFragment :
+    BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHomeBinding::inflate) {
 
+    private lateinit var genreAdapter: HomeGenreAdapter
+    private lateinit var promoAdapter: HomePromoAdapter
+    private lateinit var nowPlayingAdapter: HomePlayingMovieAdapter
     override val vm: HomeViewModel by viewModels()
-    val regionList = arrayListOf<String>(
+    private val regionList = arrayListOf<String>(
         "Aceh",
         "Bali",
         "Bangka Belitung",
@@ -59,12 +70,59 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHo
     )
 
     override fun setupViews() {
+        genreAdapter = HomeGenreAdapter()
+        promoAdapter = HomePromoAdapter()
+        nowPlayingAdapter = HomePlayingMovieAdapter()
         binding.apply {
+            vm.fetchHomeContent()
             tvHomeRegion.text = regionList.random()
+            rvHomeGenreList.apply {
+                layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                adapter = genreAdapter
+            }
         }
     }
 
     override fun setupObservers() {
+        val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+        val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        vm.apply {
+            observeMovieList().observe(viewLifecycleOwner) { data ->
+                binding.apply {
+                    vpHomePromoSlider.apply {
+                        orientation = ViewPager2.ORIENTATION_HORIZONTAL
+                        adapter = promoAdapter
+                        currentItem = Int.MAX_VALUE / 2
+                    }
+                    vpHomeNowPlaying.apply {
+                        orientation = ViewPager2.ORIENTATION_HORIZONTAL
+                        adapter = nowPlayingAdapter
+                        currentItem = Int.MAX_VALUE / 2
+                        offscreenPageLimit = 1
+                        setPageTransformer { page, position ->
+                            page.apply {
+                                translationX = -pageTranslationX * position
+                                scaleY = 1 - (0.25f * abs(position))
+                                alpha = 0.25f + (1 - abs(position))
+                            }
+                        }
+                        val itemDecoration = HorizontalMarginItemDecoration(
+                            requireContext(),
+                            R.dimen.viewpager_current_item_horizontal_margin
+                        )
+                        addItemDecoration(itemDecoration)
+                    }
+                }
+                promoAdapter.setData(data.results)
+                nowPlayingAdapter.setData(data.results)
+
+            }
+            observeGenreList().observe(viewLifecycleOwner) { data ->
+                genreAdapter.setData(data.genres)
+            }
+        }
     }
 
 }
