d78c67dae70fa7f497a4b6a8e64bd2917ae33495
StanceFreak <jombor.army67@gmail.com>
2025-06-13 21:28:01 +0700

    feat(lib & config): added new glide library and config for the api token to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,14 @@ android {
     buildTypes {
         debug {
             buildConfigField("String", "BASE_URL", localProperties["BASE_URL"].toString())
+            buildConfigField("String", "API_KEY", localProperties["API_KEY"].toString())
+            buildConfigField("String", "ACCESS_TOKEN", localProperties["ACCESS_TOKEN"].toString())
         }
         release {
-            buildConfigField("String", "BASE_URL", localProperties["BASE_URL"].toString())
             isMinifyEnabled = false
+            buildConfigField("String", "BASE_URL", localProperties["BASE_URL"].toString())
+            buildConfigField("String", "API_KEY", localProperties["API_KEY"].toString())
+            buildConfigField("String", "ACCESS_TOKEN", localProperties["ACCESS_TOKEN"].toString())
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -64,8 +68,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.navigation.fragment.ktx)
-    implementation(libs.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -82,4 +86,5 @@ dependencies {
     implementation(libs.okhttp.loggingInterceptor)
     implementation(libs.datastore)
     implementation(libs.datastore.preferences)
+    implementation(libs.glide)
 }
